d158358c37839d0fe0095256359ab64a4497edc4
quintonangus04-afk <quintonangus04@gmail.com>
2026-02-04 19:21:04 +0000

    Changed repository location

---
+++ script.js
@@ -0,0 +1,24 @@
+const sendBtn = document.getElementById('sendBtn')
+
+sendBtn.addEventListener('click', async () => {
+    let emailAdress = document.getElementById('email-input').value
+    let message = document.getElementById('text-input').value
+
+    let data = {to: emailAdress, message: message}
+
+    let response = await fetch('http://localhost:3000/send', {
+        method: 'POST',
+        headers: { 'content-type': 'application/json'},
+        body: JSON.stringify(data)
+    })
+
+    let result = await response.json()
+
+    if (result.success) {
+        window.location.href = 'success.html'
+    } else {
+        alert('Email failed to send')
+    }
+
+
+})
