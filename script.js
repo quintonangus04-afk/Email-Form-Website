0352f90c8071c13593321e1f2059de43fafc90c0
quintonangus04-afk <quintonangus04@gmail.com>
2026-02-04 21:13:35 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
 const sendBtn = document.getElementById('sendBtn')
 
 sendBtn.addEventListener('click', async () => {
-    let emailAdress = document.getElementById('email-input').value
+    let emailAddress = document.getElementById('email-input').value
     let message = document.getElementById('text-input').value
 
-    let data = {to: emailAdress, message: message}
+    let data = { to: emailAddress, message: message }
 
     let response = await fetch('https://email-backend-tljh.onrender.com/send', {
-    method: 'POST',
-    headers: { 'content-type': 'application/json' },
-    body: JSON.stringify(data)
-})
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify(data)
+    })
 
-let result = await response.json()
-
-
+    let result = await response.json()
 
     if (result.success) {
-        window.location.href = 'success.html'
+        alert("Backend received your request!")
     } else {
-        alert('Email failed to send')
+        alert("Backend responded but failed")
     }
-
-
 })
