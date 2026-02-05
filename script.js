bf918b841715fd00c3aca11269d91bd479f08bb8
quintonangus04-afk <quintonangus04@gmail.com>
2026-02-05 00:16:48 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,22 +3,24 @@ const sendBtn = document.getElementById('sendBtn')
 sendBtn.addEventListener('click', async () => {
     let emailAddress = document.getElementById('email-input').value
     let message = document.getElementById('text-input').value
-    console.log('Btn working as expected!')
-    console.log(emailAddress)
-    console.log(message)
+
+    console.log("Button clicked")
+    console.log("Email:", emailAddress)
+    console.log("Message:", message)
 
     let data = { to: emailAddress, message: message }
 
-    let response = await fetch('https://email-backend-production-972d.up.railway.app/send', {
-        method: 'POST',
-        headers: { 'content-type': 'application/json' },
+    let response = await fetch("https://email-backend-production-972d.up.railway.app/send", {
+        method: "POST",
+        mode: "cors",
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data)
     })
 
     let result = await response.json()
 
     if (result.success) {
-        alert("Backend received your request!")
+        alert("Email sent successfully!")
     } else {
         alert("Backend responded but failed")
     }
